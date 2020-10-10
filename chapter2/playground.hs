a123fb3dc5790fceb96e49a9951913b089d7c554
Passawis <official.passawis@gmail.com>
2020-10-10 22:28:43 +0700

    added learn.hs file

--- chapter2/playground.hs
+++ chapter2/playground.hs
@@ -7,3 +7,15 @@ triple :: Integer -> Integer
 
 triple x = x * 3
 -- [name of function] [parameter] [define] [body]
+
+
+main = 
+    -- infix style
+
+    10 `div` 4
+    -- normal
+    10 / 4
+
+    -- infix operators in prefix fashion
+
+    (+) 100 100
