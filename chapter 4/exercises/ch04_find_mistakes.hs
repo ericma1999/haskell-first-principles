be599c7d5b636d126abdd0ee475083ee53a9638a
Passawis <official.passawis@gmail.com>
2020-10-12 21:56:34 +0700

    added more exercises

---
+++ chapter 4/exercises/ch04_find_mistakes.hs
@@ -0,0 +1,18 @@
+module FindMistakes where
+
+    -- not True && true
+    one = not True && True
+
+
+    -- not (x = 6)
+    x = 5
+
+    two = not ( x == 6)
+
+    three = (1 * 2) > 6
+
+    -- [Merry] > [Happy]
+
+    four =
+        ["Merry"] > ["Happy"]
+
